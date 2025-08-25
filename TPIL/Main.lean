9b288ff916adb602e078f167b56c2786f97503ff
Alex <alexeusgr@gmail.com>
2025-08-25 12:11:04 +0700

    unexpected: check, inductive

--- TPIL/Main.lean
+++ TPIL/Main.lean
@@ -4,5 +4,14 @@ import Examples
 
 theorem bogus : False := by sorry
 
+#check fun x => [x]
+fun x => [x] : ?m.9 → List ?m.9
+
+-- https://lean-lang.org/doc/reference/4.21.0-rc3///Introduction/#example-boxes
+-- this does nothing:
+inductive Even : Nat → Prop where
+  | zero : Even 0
+  | plusTwo : Even n → Even (n + 2)
+
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
