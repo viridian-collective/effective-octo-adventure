24fce56b4548fe059de053a754b7a086b10b5235
Alex <alexeusgr@gmail.com>
2025-08-28 19:51:16 +0700

    test MIL intro

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,9 @@ open Lake DSL
 require aesop from git
   "https://github.com/leanprover-community/aesop.git" @ "v4.22.0-rc4"
 
+-- require mathlib from git
+--  "https://github.com/leanprover-community/mathlib4.git"
+
 package Eoa
 
 lean_lib Example
