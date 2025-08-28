72ced89966cd856d8a17ca53d03936a714a47d42
Alex <alexeusgr@gmail.com>
2025-08-28 08:47:23 +0700

    clean up before rerunning init

--- Main.lean
+++ Main.lean
@@ -1,16 +1,16 @@
 -- import Examples
 
-  #eval s!"The answer is {40 + 2}"
+#eval s!"The answer is {40 + 2}"
 
-  theorem bogus : False := by sorry
+theorem bogus : False := by sorry
 
-  -- errs!
-  -- #check fun x => [x]
-  -- fun x => [x] : ?m.9 → List ?m.9
+-- errs!
+--#check fun x => [x]
+--fun x => [x] : ?m.9 → List ?m.9
 
-  -- https://lean-lang.org/doc/reference/4.21.0-rc3///Introduction/#example-boxes
-  -- this does nothing:
-  inductive Even : Nat → Prop where
+-- https://lean-lang.org/doc/reference/4.21.0-rc3///Introduction/#example-boxes
+-- this does nothing:
+inductive Even : Nat → Prop where
   | zero : Even 0
   | plusTwo : Even n → Even (n + 2)
 
