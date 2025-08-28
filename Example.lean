53215320b80aa84ea582a9cc3bd4dc3f9417b7e2
Alex <alexeusgr@gmail.com>
2025-08-28 19:28:22 +0700

    cherry-pick

--- Example.lean
+++ Example.lean
@@ -1,4 +1,5 @@
 import Example.Basic
+import Example.DependentTypeTheory
 import Aesop
 
 def name : String := "Alex"
--- Main.lean
+++ Main.lean
@@ -1,10 +1,35 @@
 import Example
+/- 
+- [ ] find or implement a proof for a famous theorem
+- [ ] [The Schröder-Bernstein Theorem](https://leanprover-community.github.io/mathematics_in_lean/C04_Sets_and_Functions.html#the-schroder-bernstein-theorem) 
+- [ ] in Economics: [A Definition of Subjective Probability](https://www.jstor.org/stable/2991295)
+- [ ] in Chaos: The butterfly effect, a key feature of deterministic chaos, describes how small differences in initial conditions lead to vastly different outcomes over time, making long-term prediction impossible even in deterministic systems.
+- [ ] in Network Models: https://en.wikipedia.org/wiki/Braess'_paradox
+
+
+## Module 300: Innovate
+- [eUTXO](https://iohk.io/en/research/library/papers/the-extended-utxo-model/) in lean
+- eUTXO as a component of a socio-economic system
+- WNF "critical juncture" as a phase transition in a socio-economic system 
+- blockchain transactions and state of ledger as datapoints for validating theories around economic and other kinds of institutions
+
+## Module 400: Connect
+- [x] [zulip](https://leanprover.zulipchat.com/)
+  - [ ] introduce yourself
+- [ ] Office hours
+  - [ ] write email,
+  - [ ] attend 
+- [ ] active projects
+  - [ ] Cardano.org
+  - [ ] andamio.io
+  - [ ] [FLT](https://github.com/ImperialCollegeLondon/FLT)
+  - [ ] nix scaffold
+  -/
 
 -- errs!
 --#check fun x => [x]
 --fun x => [x] : ?m.9 → List ?m.9
 
--- https://lean-lang.org/doc/reference/4.21.0-rc3///Introduction/#example-boxes
 -- what does this even mean? 
 inductive Even : Nat → Prop where
   | zero : Even 0
