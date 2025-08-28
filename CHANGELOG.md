ad740641357983181e7f4cb9be195bec545a0b4d
Alex <alexeusgr@gmail.com>
2025-08-28 07:52:48 +0700

    add QoL and IO program example

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+### 0.1.0 - Initial Setup and Lean Language Installation
+
+* Set up Lean language on the system, encountering two errors which are documented in GitHub issue #1.
+* Initial thoughts on using Lean to create a model of a tokenomic system, exploring connections between personal value systems, ethical frameworks, and collective value systems.
+* Explored the idea of modeling institutions as smart contracts and transactions, and what it means for a governance system to be inclusive.
+* Identified the need to explore Cardano's transaction model (eUTXO) and its relationship to tokenomics.
---
+++ Feline.lean
@@ -0,0 +1,50 @@
+/-
+
+This is the worked example from the Functional Programming in Lean book:
+https://lean-lang.org/functional_programming_in_lean/Hello___-World___/#hello-world
+
+to test create two text files and run `lean --run Feline.lean file1.txt file2.txt` to see two files concatenated.
+
+-/
+
+def bufsize : USize := 20 * 1024
+
+partial def dump (stream : IO.FS.Stream) : IO Unit := do
+  let buf ← stream.read bufsize
+  if buf.isEmpty then
+    pure ()
+  else
+    let stdout ← IO.getStdout
+    stdout.write buf
+    dump stream
+
+def fileStream (filename : System.FilePath) : IO (Option IO.FS.Stream) := do
+  let fileExists ← filename.pathExists
+  if not fileExists then
+    let stderr ← IO.getStderr
+    stderr.putStrLn s!"File not found: {filename}"
+    pure none
+  else
+    let handle ← IO.FS.Handle.mk filename IO.FS.Mode.read
+    pure (some (IO.FS.Stream.ofHandle handle))
+
+def process (exitCode : UInt32) (args : List String) : IO UInt32 := do
+  match args with
+  | [] => pure exitCode
+  | "-" :: args =>
+    let stdin ← IO.getStdin
+    dump stdin
+    process exitCode args
+  | filename :: args =>
+    let stream ← fileStream ⟨filename⟩
+    match stream with
+    | none =>
+      process 1 args
+    | some stream =>
+      dump stream
+      process exitCode args
+
+def main (args : List String) : IO UInt32 :=
+  match args with
+  | [] => process 0 ["-"]
+  | _ =>  process 0 args
